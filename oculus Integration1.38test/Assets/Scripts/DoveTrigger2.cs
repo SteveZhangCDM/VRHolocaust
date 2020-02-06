1298ccccf35858c94ab68cc4e9e0093405429193
SteveZhangCDM <59717016+SteveZhangCDM@users.noreply.github.com>
2020-02-05 21:39:10 -0800

    Added second scene; new script for switch scene by using scene management.

---
+++ oculus Integration1.38test/Assets/Scripts/DoveTrigger2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoveTrigger2 : MonoBehaviour
+{
+    public Animator anima;
+
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.name == "GrabVolumeBig")
+        {
+            Debug.Log("Hit");
+            anima.Play("Dove#2");
+        }
+    }
+}
