c18ff957916f0d49c9121dc6033fe6db5e967e0d
SteveZhangCDM <59717016+SteveZhangCDM@users.noreply.github.com>
2020-02-20 10:59:05 -0800

    new 3 scene made by HDR background; 2 types of camera move for beginning

---
+++ oculus Integration1.38test/Assets/Scripts/openingAnimationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openingAnimationController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> cameras = new List<GameObject>();
+
+
+    public static openingAnimationController instance = null;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void StartNextCamera()
+    {
+        if(cameras.Count > 0)
+        {
+            cameras[0].SetActive(true);
+        }
+        
+    }
+
+    public void DestroyCamera(GameObject camera)
+    {
+        cameras.RemoveAt(0);
+        Destroy(camera);
+        StartNextCamera();
+    }
+
+   
+}
